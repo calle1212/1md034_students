3ea91abdeff58c963207ed5bf5c4b1c9f392a30c
calle1212 <carl.hasselberg.6020@student.uu.se>
2020-02-03 13:50:01 +0100

    nothing

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -17,7 +17,7 @@ this.nameAndKcal = function(){
 }
 
 
-menuItem1 = new menuItem("Big boy burger", "tasty burgers made for big boys", 1400, false, true, true, "img/burg1.jpeg");
+menuItem1 = new menuItem("Big boy burger", "tasty burgers made for big boys", 1400, false , true, true, "img/burg1.jpeg");
 menuItem2 = new menuItem("Homeboy burger", "For the homeboys ONLY", 2300, false, true, true, "img/burg2.jpeg");
 menuItem3 = new menuItem("Classy burger", "made for the burger connoisseur", 1000, false, false, true, "img/burg3.jpeg");
 menuItem4 = new menuItem("burg", "...it's a burger", 1100, false, false, false, "img/burg3.jpeg");
