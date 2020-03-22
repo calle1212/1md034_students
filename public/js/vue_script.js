20f09c0b6b6f1696e0006323499416d672e90936
calle1212 <carl.hasselberg.6020@student.uu.se>
2020-03-22 01:05:12 +0100

    working with sockets

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,3 +1,5 @@
+'use strict';
+const socket = io();
 
 /*
 const vm = new Vue({
@@ -16,43 +18,43 @@ const vm1 = new Vue({
 })
 
 const vm2 = new Vue({
-  el: '#b2',
-  data: {
-    burgerName: menuItem2.name
-  }
+el: '#b2',
+data: {
+burgerName: menuItem2.name
+}
 })
 const vm3 = new Vue({
-  el: '#b3',
-  data: {
-    burgerName: menuItem3.name
-  }
+el: '#b3',
+data: {
+burgerName: menuItem3.name
+}
 })
 const vm4 = new Vue({
-  el: '#b4',
-  data: {
-    burgerName: menuItem4.name
-  }
+el: '#b4',
+data: {
+burgerName: menuItem4.name
+}
 })
 const vm5 = new Vue({
-  el: '#b5',
-  data: {
-    burgerName: menuItem5.name
-  }
+el: '#b5',
+data: {
+burgerName: menuItem5.name
+}
 })
 
 */
 
 /*
- ______HTML CODE____
-<div id="myID">
-    <h1>Välj en burgare</h1>
-    <p id="b1"> {{ burgerName }} </p>
-    <p id="b2"> {{ burgerName }}</p>
-    <p id="b3"> {{ burgerName }}</p>
-    <p id="b4">{{ burgerName }} </p>
-    <p id="b5"> {{ burgerName }} </p>
+  ______HTML CODE____
+  <div id="myID">
+  <h1>Välj en burgare</h1>
+  <p id="b1"> {{ burgerName }} </p>
+  <p id="b2"> {{ burgerName }}</p>
+  <p id="b3"> {{ burgerName }}</p>
+  <p id="b4">{{ burgerName }} </p>
+  <p id="b5"> {{ burgerName }} </p>
 
-</div>
+  </div>
 
 */
 
@@ -63,32 +65,32 @@ const vm5 = new Vue({
 //let menu = [menuItem1, menuItem2, menuItem3, menuItem4, menuItem5];
 
 /*
-var loop = new Vue({
-    el: '#grid',
-  data: {
-   menu 
-  }
-})
-*/
-/*
-const vm5 = new Vue({
-  el: '#burger2',
-  data: {
-      burger: menu[1]
-  }
-})
-*/
-
-
-
-/*
-const burgerz = new Vue({
+  var loop = new Vue({
   el: '#grid',
   data: {
-      
+  menu 
+  }
+  })
+*/
+/*
+  const vm5 = new Vue({
+  el: '#burger2',
+  data: {
+  burger: menu[1]
+  }
+  })
+*/
+
+
+
+/*
+  const burgerz = new Vue({
+  el: '#grid',
+  data: {
+  
   
   }
-})
+  })
 */
 
 const info = new Vue({
@@ -97,45 +99,93 @@ const info = new Vue({
         menu: food,
         
         vburgers: [],
-       
+        
         vname: '',
         vemail: '',
-        vstreet: '',
-        vhouse: '',
+        // vstreet: '',
+        // vhouse: '',
         vpay_op: 'Credit Card',
         vgender: 'Male',
-         
+        orders: {},
+        
         
         clicked: false
     },
-    
-  methods: {
-        buttonClicked: function() {
-            console.log("button clicked!");
-            /*
-            console.log("name: " + this.vname);
-            console.log("email: " + this.vemail);
-            console.log("street: " + this.vstreet);
-            console.log("house: " + this.vstreet);
-            console.log("pay_op: " + this.vpay_op);
-            console.log("gender: " + this.vgender);
-            console.log("burgers: " + this.vburgers);
 
-            console.log(this.clicked);
-            */
-            this.clicked = !this.clicked;
+
+  created: function() {
+    /* When the page is loaded, get the current orders stored on the server.
+     * (the server's code is in app.js) */
+    socket.on('initialize', function(data) {
+      this.orders = data.orders;
+    }.bind(this));
+
+    /* Whenever an addOrder is emitted by a client (every open map.html is
+     * a client), the server responds with a currentQueue message (this is
+     * defined in app.js). The message's data payload is the entire updated
+     * order object. Here we define what the client should do with it.
+     * Spoiler: We replace the current local order object with the new one. */
+    socket.on('currentQueue', function(data) {
+      this.orders = data.orders;
+    }.bind(this));
+  },
+        
+        methods: {
+            buttonClicked: function() {
+                console.log("button clicked!");
+                /*
+                  console.log("name: " + this.vname);
+                  console.log("email: " + this.vemail);
+                  console.log("street: " + this.vstreet);
+                  console.log("house: " + this.vstreet);
+                  console.log("pay_op: " + this.vpay_op);
+                  console.log("gender: " + this.vgender);
+                  console.log("burgers: " + this.vburgers);
+
+                  console.log(this.clicked);
+                */
+                this.clicked = !this.clicked;
+                
+                
+            },
+            fakeSend: function() {
+                alert("Order Sent!");
+                location.reload(); 
+
+                
+            },
             
-       
-        },
-      fakeSend: function() {
-          alert("Order Sent!");
-          location.reload(); 
-
-          
-      }
-      
-  }
-})
+            getNext: function() {
+                /* This function returns the next available key (order number) in
+                 * the orders object, it works under the assumptions that all keys
+                 * are integers. */
+                let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+                    return Math.max(last, next);
+                }, 0);
+                return lastOrder + 1;
+            },
+            addOrder: function(event) {
+                /* When you click in the map, a click event object is sent as parameter
+                 * to the function designated in v-on:click (i.e. this one).
+                 * The click event object contains among other things different
+                 * coordinates that we need when calculating where in the map the click
+                 * actually happened. */
+                let offset = {
+                    x: event.currentTarget.getBoundingClientRect().left,
+                    y: event.currentTarget.getBoundingClientRect().top,
+                };
+                socket.emit('addOrder', {
+                    orderId: this.getNext(),
+                    details: {
+                        x: event.clientX - 10 - offset.x,
+                        y: event.clientY - 10 - offset.y,
+                    },
+                    orderItems: ['Beans', 'Curry'],
+                });
+            }
+            
+        }
+    })
                         
 
 /*
